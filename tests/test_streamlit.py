7d34ff594254c801652f2f41975b03dbab2464ee
hrg <hrgupta@outlook.com>
2021-06-13 05:39:30 -0500

    modify file path

--- tests/test_streamlit.py
+++ tests/test_streamlit.py
@@ -7,7 +7,7 @@ def test_input_url():
 
 def test_class():
     response = dog.get_model_output("https://www.thesprucepets.com/thmb/wpN_ZunUaRQAc_WRdAQRxeTbyoc=/4231x2820/filters:fill(auto,1)/adorable-white-pomeranian-puppy-spitz-921029690-5c8be25d46e0fb000172effe.jpg")
-    with open(os.path.join(os.getcwd(), '.\dogapp\dog_names.txt')) as file:
+    with open(os.path.join(os.getcwd(), './dogapp/dog_names.txt')) as file:
         dog_names = file.read()
     dog_names = dog_names.split('\n')
     assert response["data"]["prediction"][0]["class"] in dog_names
