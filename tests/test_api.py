451e775d0d8367fd62a411773b7b3e81b48b8b3c
hrg <hrgupta@outlook.com>
2021-06-13 04:50:40 -0500

    change test code

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,14 @@
-import requests
+from fastapi.testclient import TestClient
+
+from dogapp.app import app
+client = TestClient(app)
 
 def test_index():
-    response = requests.get("http://localhost:5000/")
+    response = client.get("/")
     assert response.status_code == 200
 
 def test_predict():
     body = {"urls": ["https://www.thesprucepets.com/thmb/wpN_ZunUaRQAc_WRdAQRxeTbyoc=/4231x2820/filters:fill(auto,1)/adorable-white-pomeranian-puppy-spitz-921029690-5c8be25d46e0fb000172effe.jpg"]}
-    response = requests.post("http://localhost:5000/predict", json=body)
+    response = client.post("/predict", json=body)
     assert response.status_code == 200
     assert "data" in response.json()
